99dae6e04a85b6d36e288c7297b62d59f0c36820
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-10-26 16:59:16 -0300

    aula sobre calculos em javascript

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let anoAtual = 2023;
+let anoNascimento = 0;
+let resultado = 0;
+
+anoNascimento = prompt("digite o ano de nascimento");
+
+anoNascimento = parseInt(anoNascimento);
+
+resultado = anoAtual - anoNascimento;
+
+alert("você tem: " + resultado + " anos porque nasceu em " + anoNascimento);
