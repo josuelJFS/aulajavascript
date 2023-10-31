6fcec961b796add95d7538a281856229d985d4e9
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-10-31 16:03:13 -0300

    teste

--- javascript.js
+++ javascript.js
@@ -1,11 +1,1 @@
-let idade = 0;
-
-idade = parseInt(prompt("informe sua idade"));
-
-if (idade >= 18) {
-  //entao faça isso
-  alert("você é maior de idade");
-} else {
-  //se não faça isso
-  alert("você não é maior de idade");
-}
+let nota1 = 0;
