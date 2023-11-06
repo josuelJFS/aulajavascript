74627fbc249b872f25cf362fe55ba41950d0f822
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-11-06 12:51:13 -0300

    array

--- javascript.js
+++ javascript.js
@@ -1,23 +1,19 @@
-let nota1 = 0;
-let nota2 = 0;
-let nota3 = 0;
-let resultado = 0;
+let arrayCores = [
+  "vermelho",
+  "azul",
+  "verde",
+  "amarelo",
+  "roxo",
+  "laranja",
+  "rosa",
+  "marrom",
+  "cinza",
+  "preto",
+  "branco",
+];
+let numeroLista = 0;
 
-nota1 = parseInt(prompt("Informe a primeira nota"));
-nota2 = parseInt(prompt("Informe a segunda nota"));
-nota3 = parseInt(prompt("Informe a terceira nota"));
-
-resultado = (nota1 + nota2 + nota3) / 3;
-resultado = parseInt(resultado);
-console.log(resultado);
-if (resultado > 0 && resultado <= 5) {
-  alert("Reprovado");
-}
-
-if (resultado == 6) {
-  alert("recuperação");
-}
-
-if (resultado > 6) {
-  alert("Aprovado");
-}
+arrayCores.forEach((cor) => {
+  numeroLista++;
+  console.log("cor " + numeroLista + " = " + cor);
+});
