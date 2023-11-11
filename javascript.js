81945b47182a795489271b1dac6e710f63da00d0
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-11-11 15:09:21 -0300

    calculadora

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
-function alertaBtn() {
-  let input = document.getElementById("nome");
-  document.body.style.backgroundColor = input.value;
+function calculadora() {
+  let resultado = document.getElementById("resultado");
+  let resultadoDoCalculo = 0;
+  let operacoes = document.getElementById("operacoes").value;
+  let input1 = parseInt(document.getElementById("input1").value);
+  let input2 = parseInt(document.getElementById("input2").value);
+
+  if (operacoes == "+") {
+    resultadoDoCalculo = input1 + input2;
+  }
+
+  if (operacoes == "-") {
+    resultadoDoCalculo = input1 - input2;
+  }
+
+  if (operacoes == "*") {
+    resultadoDoCalculo = input1 * input2;
+  }
+
+  if (operacoes == "/") {
+    resultadoDoCalculo = input1 / input2;
+  }
+  resultado.innerText = resultadoDoCalculo;
 }
-
-function main() {}
-
-window.onload = function () {
-  main();
-};
