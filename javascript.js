b48ec8a2f75422e299c2b45d0f016fc94cf4306b
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-11-11 10:41:42 -0300

    init

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
-function main() {}
+function main() {
+  let anoNascimento = 0;
+  let btn = document.getElementById("btn");
+  anoNascimento = document.getElementById("anoNascimentoInput");
+
+  btn.onclick = function () {
+    let idade = 2023 - parseInt(anoNascimento.value);
+    document.getElementById("resultado").innerText =
+      "sua idade é " + idade + " anos";
+  };
+}
 
 window.onload = function () {
   main();
