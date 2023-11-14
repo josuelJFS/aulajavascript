0ea5ec48383fcef465b3a2c863b6fc1979229d10
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-11-14 20:06:54 -0300

    calculoAluno

--- javascript.js
+++ javascript.js
@@ -1,24 +1,39 @@
-function calculadora() {
-  let resultado = document.getElementById("resultado");
-  let resultadoDoCalculo = 0;
-  let operacoes = document.getElementById("operacoes").value;
-  let input1 = parseInt(document.getElementById("input1").value);
-  let input2 = parseInt(document.getElementById("input2").value);
+function mediaDoAluno() {
+  let nota1 = parseInt(document.getElementById("nota1").value);
+  let nota2 = parseInt(document.getElementById("nota2").value);
+  let nota3 = parseInt(document.getElementById("nota3").value);
+  let nota4 = parseInt(document.getElementById("nota4").value);
+  let resultadoDoAluno = "";
+  let divResultado = document.getElementById("resultado");
 
-  if (operacoes == "+") {
-    resultadoDoCalculo = input1 + input2;
+  resultadoDoAluno = calculoMediaDoAluno(nota1, nota2, nota3, nota4);
+  divResultado.innerText = resultadoDoAluno;
+  if (resultadoDoAluno == "Aprovado") {
+    divResultado.style.color = "#529B25";
+  }
+  if (resultadoDoAluno == "Reprovado") {
+    divResultado.style.color = "#FD1010";
+  }
+  if (resultadoDoAluno == "Recuperação") {
+    divResultado.style.color = "#FF9B06";
+  }
+}
+
+function calculoMediaDoAluno(nota1, nota2, nota3, nota4) {
+  let media = (nota1 + nota2 + nota3 + nota4) / 4;
+  media = Math.round(media);
+  if (media >= 7) {
+    document.getElementById("notaDoaluno").innerText = "Média = " + media;
+    return "Aprovado";
   }
 
-  if (operacoes == "-") {
-    resultadoDoCalculo = input1 - input2;
+  if (media <= 4) {
+    document.getElementById("notaDoaluno").innerText = "Média = " + media;
+    return "Reprovado";
   }
 
-  if (operacoes == "*") {
-    resultadoDoCalculo = input1 * input2;
+  if (media > 4 && media < 7) {
+    document.getElementById("notaDoaluno").innerText = "Média = " + media;
+    return "Recuperação";
   }
-
-  if (operacoes == "/") {
-    resultadoDoCalculo = input1 / input2;
-  }
-  resultado.innerText = resultadoDoCalculo;
 }
