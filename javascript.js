3be55c5642ab7fbf7e23c1baed4fb14fde0a0b81
c1326438 josuel fernandes silva <c1326438@interno.bb.com.br>
2023-11-21 15:09:22 -0300

    ajsutes

--- javascript.js
+++ javascript.js
@@ -1,39 +1,25 @@
-function mediaDoAluno() {
+function media() {
   let nota1 = parseInt(document.getElementById("nota1").value);
   let nota2 = parseInt(document.getElementById("nota2").value);
   let nota3 = parseInt(document.getElementById("nota3").value);
   let nota4 = parseInt(document.getElementById("nota4").value);
-  let resultadoDoAluno = "";
-  let divResultado = document.getElementById("resultado");
-
-  resultadoDoAluno = calculoMediaDoAluno(nota1, nota2, nota3, nota4);
-  divResultado.innerText = resultadoDoAluno;
-  if (resultadoDoAluno == "Aprovado") {
-    divResultado.style.color = "#529B25";
-  }
-  if (resultadoDoAluno == "Reprovado") {
-    divResultado.style.color = "#FD1010";
-  }
-  if (resultadoDoAluno == "Recuperação") {
-    divResultado.style.color = "#FF9B06";
-  }
-}
-
-function calculoMediaDoAluno(nota1, nota2, nota3, nota4) {
-  let media = (nota1 + nota2 + nota3 + nota4) / 4;
+  let media = 0;
+  media = (nota1 + nota2 + nota3 + nota4) / 4;
   media = Math.round(media);
+  console.log(media);
+
   if (media >= 7) {
-    document.getElementById("notaDoaluno").innerText = "Média = " + media;
-    return "Aprovado";
+    document.getElementById("resultado").innerText = "Aprovado";
+    document.getElementById("resultado").style.color = "#529B25";
   }
 
   if (media <= 4) {
-    document.getElementById("notaDoaluno").innerText = "Média = " + media;
-    return "Reprovado";
+    document.getElementById("resultado").innerText = "Reprovado";
+    document.getElementById("resultado").style.color = "#FD1010";
   }
 
   if (media > 4 && media < 7) {
-    document.getElementById("notaDoaluno").innerText = "Média = " + media;
-    return "Recuperação";
+    document.getElementById("resultado").innerText = "Recuperação";
+    document.getElementById("resultado").style.color = "#FF9B06";
   }
 }
